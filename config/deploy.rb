fa234e4ccbc3dde16db801b15ac0975a51ee8a2b
raysango <remongeorge@live.com>
2015-01-05 02:54:45 +0000

    Home view update

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,11 +5,3 @@ set :scm_passphrase, ""
 
 set :whenever_environment, defer { production }
 require "whenever/capistrano"
-
-namespace :todo do
-  desc "Delete items older than seven days"
-  task delete_items: :environment do
-    Item.where("created_at <= ?", Time.now - 7.days).destroy_all
-  end
-
-end
